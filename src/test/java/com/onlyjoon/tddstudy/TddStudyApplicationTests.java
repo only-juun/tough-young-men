bba9efb7e12c0f2c9eb345e41244d34ed86acf36
남환준 <nhj7334@icloud.com>
2024-04-26 19:50:28 +0900

    🎉 init: 최초 커밋

---
+++ src/test/java/com/onlyjoon/tddstudy/TddStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlyjoon.tddstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
